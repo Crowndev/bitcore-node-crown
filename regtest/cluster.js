8dc6a7d4fd3a3f0bfcd86133e7f16ba88137d46e
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2018-11-05 04:37:06 +0800

    Replace bitcoind-rpc with crownd-rpc

--- regtest/cluster.js
+++ regtest/cluster.js
@@ -4,7 +4,7 @@ var path = require('path');
 var async = require('async');
 var spawn = require('child_process').spawn;
 
-var BitcoinRPC = require('bitcoind-rpc');
+var BitcoinRPC = require('crownd-rpc');
 var rimraf = require('rimraf');
 var bitcore = require('bitcore-lib-crown');
 var chai = require('chai');
